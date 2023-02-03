b97a2cafd7a6f564f21355bb3778e411d277c4f2
Anuj Pundalik <anuj.pundalik@contentstack.com>
2023-02-03 15:51:13 +0530

    Add check if env variables is present in gatsby function

--- src/api/hello.js
+++ src/api/hello.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
+  if (process.env.TEST_VARIABLE !== "TEST_VALUE") {
+    throw new Error("env variable not present");
+  }
+
   res
     .status(200)
     .json({
